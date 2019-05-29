f3cf39ba38a0021ecb4dc0bc92f3560db31a9d37
Brian O'Sullivan <bosullivan@transfermate.com>
2019-05-29 15:10:57 +0100

    Updated portfolio deployment function for lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,17 +1,22 @@
 import boto3
-from botocore.client import Config
+import StringIO
 import zipfile
+import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
 
-build_bucket = s3.Bucket('portfoliobuild.bos.info')
-portfolio_bucket = s3.Bucket('portfolio.bos.info')
-
-# On Windows, this will need to be a different location than /tmp
-build_bucket.download_file('portfolio.zip', 'c:/Users/bosullivan/portfolio.zip')
-
-with zipfile.ZipFile('c:/Users/bosullivan/portfolio.zip') as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        target_bucket.upload_fileobj(obj, nm)
-        target_bucket.Object(nm).Acl().put(ACL='public-read')
+    s3 = boto3.resource('s3')
+    
+    build_bucket = s3.Bucket('portfoliobuild.bos.info')
+    portfolio_bucket = s3.Bucket('portfolio.bos.info')
+    
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm)
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+            
+        return 'Hello from Lambda'
