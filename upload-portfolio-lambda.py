575bba1678b01cd6cfde1828992561d10bdc3973
Brian O'Sullivan <bosullivan@transfermate.com>
2019-05-31 10:32:14 +0100

    Added SNS topic publish

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,17 +6,25 @@ import mimetypes
 def lambda_handler(event, context):
 
     s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
     
-    build_bucket = s3.Bucket('portfoliobuild.bos.info')
-    portfolio_bucket = s3.Bucket('portfolio.bos.info')
+    topic = sns.Topic('arn:aws:sns:eu-west-1:889877181860:deployPortfolioTopic')
     
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    try:
+        build_bucket = s3.Bucket('portfoliobuild.bos.info')
+        portfolio_bucket = s3.Bucket('portfolio.bos.info')
+        
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm)
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+                
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully!")
     
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm)
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-            
-        return 'Hello from Lambda'
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="The Portfolio was not deployed successfully :(")
+        raise
